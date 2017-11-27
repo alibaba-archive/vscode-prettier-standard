62d358ce16894183dede9b25fc185975d4e25356
isayme <isaymeorg@gmail.com>
2017-11-27 23:02:37 +0800

    :tada: working version

--- extension.js
+++ extension.js
@@ -1,19 +1,77 @@
-const vscode = require('vscode')
+const { commands, window, Selection, Position } = require('vscode')
+const eslintConfigStandard = require('eslint-config-standard')
+const prettierESLint = require('prettier-eslint')
+
+function formatAndReplace (range) {
+  const activeTextEditor = window.activeTextEditor
+  const text = activeTextEditor.document.getText(range)
+  try {
+    const formatted = prettierESLint({
+      text: text,
+      eslintConfig: eslintConfigStandard
+    })
+    activeTextEditor
+      .edit(editor => {
+        editor.replace(range, formatted)
+      })
+      .then(
+        () => {
+          window.showInformationMessage('format success!')
+        },
+        err => {
+          window.showErrorMessage(err.message)
+        }
+      )
+  } catch (err) {
+    window.showErrorMessage(err.message)
+  }
+}
 
 function activate (context) {
   console.log(
     'Congratulations, your extension "vscode-prettier-standard" is now active!'
   )
 
-  let prettierDocument = vscode.commands.registerCommand(
+  let prettierDocument = commands.registerCommand(
     'extension.prettierDocument',
     function () {
+      const activeTextEditor = window.activeTextEditor
+      if (!activeTextEditor) {
+        return
+      }
+
+      const document = activeTextEditor.document
+      const lineCount = document.lineCount
+      if (lineCount < 1) {
+        return
+      }
+
+      const start = new Position(0, 0)
+      const lastLine = document.lineAt(document.lineCount - 1)
+      const end = new Position(
+        activeTextEditor.document.lineCount,
+        lastLine.text.length
+      )
+      const selection = new Selection(start, end)
+
+      formatAndReplace(selection)
     }
   )
 
-  let prettierSelection = vscode.commands.registerCommand(
+  let prettierSelection = commands.registerCommand(
     'extension.prettierSelection',
     function () {
+      const activeTextEditor = window.activeTextEditor
+      if (!activeTextEditor) {
+        return
+      }
+
+      const selection = activeTextEditor.selection
+      if (!selection) {
+        return
+      }
+
+      formatAndReplace(selection)
     }
   )
 
