bb16afeb4ca6eeb512a5ce1f4834064b762c7213
isayme <isaymeorg@gmail.com>
2017-11-27 17:11:09 +0800

    register commands and remove comments

--- extension.js
+++ extension.js
@@ -1,30 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
+function activate (context) {
+  console.log(
+    'Congratulations, your extension "vscode-prettier-standard" is now active!'
+  )
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-prettier-standard" is now active!');
+  let prettierDocument = vscode.commands.registerCommand(
+    'extension.prettierDocument',
+    function () {
+    }
+  )
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+  let prettierSelection = vscode.commands.registerCommand(
+    'extension.prettierSelection',
+    function () {
+    }
+  )
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(prettierDocument)
+  context.subscriptions.push(prettierSelection)
 }
-exports.activate = activate;
+exports.activate = activate
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+function deactivate () {}
+exports.deactivate = deactivate
