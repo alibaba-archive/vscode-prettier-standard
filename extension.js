4965aca20cf3c64846c6c02ef790dbbca8893ae8
蒋巍 <649581473@qq.com>
2017-12-02 18:18:56 +0800

    feat: Support statusBar item button

--- extension.js
+++ extension.js
@@ -1,84 +1,114 @@
-const { commands, window, Selection, Position } = require('vscode')
+/* ---------------------------------------------------------
+ * Copyright (C) Teambition Corporation. All rights reserved.
+ *-------------------------------------------------------- */
+
+const { commands, window, Selection, Position, StatusBarAlignment } = require('vscode')
 const eslintConfigStandard = require('eslint-config-standard')
 const prettierESLint = require('prettier-eslint')
 
-function formatAndReplace (range) {
-  const activeTextEditor = window.activeTextEditor
-  const text = activeTextEditor.document.getText(range)
-  try {
-    const formatted = prettierESLint({
-      text: text,
-      eslintConfig: eslintConfigStandard
-    })
-    activeTextEditor
-      .edit(editor => {
-        editor.replace(range, formatted)
+function activate(context) {
+  console.log(
+    'Congratulations, your extension "vscode-prettier-standard" is now active!'
+  )
+  const controller = new FormatController(commands, window)
+  const prettierDocumentCommend = controller.prettierDocument
+  const prettierSelectionCommend = controller.prettierSelection
+  const button = controller.Button
+
+  context.subscriptions.push(prettierDocumentCommend)
+  context.subscriptions.push(prettierSelectionCommend)
+  context.subscriptions.push(button)
+}
+
+class FormatController {
+  constructor(commands, window) {
+    this.EXTENSION_NAME = 'Prettier'
+    this.PRETTIER_DOCUMENT = 'extension.prettierDocument'
+    this.PRETTIER_SELECTION = 'extension.prettierSelection'
+
+    this.window = window
+    this.commands = commands
+  }
+
+  get prettierDocument() {
+    this.commands.registerCommand(
+      this.PRETTIER_DOCUMENT,
+      () => {
+        const activeTextEditor = this.window.activeTextEditor
+        if (!activeTextEditor) {
+          return
+        }
+
+        const document = activeTextEditor.document
+        const lineCount = document.lineCount
+        if (lineCount < 1) {
+          return
+        }
+
+        const start = new Position(0, 0)
+        const lastLine = document.lineAt(document.lineCount - 1)
+        const end = new Position(
+          activeTextEditor.document.lineCount,
+          lastLine.text.length
+        )
+        const selection = new Selection(start, end)
+
+        this._formatAndReplace(selection)
+      }
+    )
+  }
+
+  get prettierSelection() {
+    this.commands.registerCommand(
+      this.PRETTIER_SELECTION,
+      function () {
+        const activeTextEditor = this.window.activeTextEditor
+        if (!activeTextEditor) {
+          return
+        }
+
+        const selection = activeTextEditor.selection
+        if (!selection) {
+          return
+        }
+
+        this._formatAndReplace(selection)
+      }
+    )
+  }
+
+  _formatAndReplace(range) {
+    const activeTextEditor = window.activeTextEditor
+    const text = activeTextEditor.document.getText(range)
+    try {
+      const formatted = prettierESLint({
+        text: text,
+        eslintConfig: eslintConfigStandard
       })
-      .then(
+      activeTextEditor
+        .edit(editor => {
+          editor.replace(range, formatted)
+        })
+        .then(
         () => {
           window.showInformationMessage('format success!')
         },
         err => {
           window.showErrorMessage(err.message)
         }
-      )
-  } catch (err) {
-    window.showErrorMessage(err.message)
+        )
+    } catch (err) {
+      window.showErrorMessage(err.message)
+    }
+  }
+
+  get Button() {
+    const _statusBarItem = this.window.createStatusBarItem(StatusBarAlignment.Right)
+    _statusBarItem.text = this.EXTENSION_NAME
+    _statusBarItem.command = this.PRETTIER_DOCUMENT
+    _statusBarItem.show()
+    return _statusBarItem
   }
 }
 
-function activate (context) {
-  console.log(
-    'Congratulations, your extension "vscode-prettier-standard" is now active!'
-  )
-
-  let prettierDocument = commands.registerCommand(
-    'extension.prettierDocument',
-    function () {
-      const activeTextEditor = window.activeTextEditor
-      if (!activeTextEditor) {
-        return
-      }
-
-      const document = activeTextEditor.document
-      const lineCount = document.lineCount
-      if (lineCount < 1) {
-        return
-      }
-
-      const start = new Position(0, 0)
-      const lastLine = document.lineAt(document.lineCount - 1)
-      const end = new Position(
-        activeTextEditor.document.lineCount,
-        lastLine.text.length
-      )
-      const selection = new Selection(start, end)
-
-      formatAndReplace(selection)
-    }
-  )
-
-  let prettierSelection = commands.registerCommand(
-    'extension.prettierSelection',
-    function () {
-      const activeTextEditor = window.activeTextEditor
-      if (!activeTextEditor) {
-        return
-      }
-
-      const selection = activeTextEditor.selection
-      if (!selection) {
-        return
-      }
-
-      formatAndReplace(selection)
-    }
-  )
-
-  context.subscriptions.push(prettierDocument)
-  context.subscriptions.push(prettierSelection)
-}
 exports.activate = activate
-
-function deactivate () {}
-exports.deactivate = deactivate
